90c18042593145fc01e65653d318ce17910a2268
arichshea <65359853+arichshea@users.noreply.github.com>
2020-07-12 22:18:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
 class Institution {
 	
 	public $name;
+	public $control;
 	public $stateCode;
+	public $combinePolicySource
 	public $policyType;
 	public $sourceLink;
 	
 	public function __construct( $databaseArray ) {
 		$this->name = $databaseArray[0];
-		$this->stateCode = $databaseArray[1];
-		$this->policyType = $databaseArray[2];
-		$this->sourceLink = $databaseArray[3];
-		$this->getCoordinates();
+		$this->control = $databaseArray[1];
+		$this->stateCode = $databaseArray[2];
+		$this->combinePolicySource = $databaseArray[3];
 	}
 	
 	
@@ -26,6 +27,7 @@ class Institution {
 class Tracker {
 	
 	public $fullData;
+	public $fullDataArray;
 	
 	public function getData() {
 		$myfile = fopen("data-w8lLG.csv", "r") or die("Unable to open file!");
@@ -35,17 +37,45 @@ class Tracker {
 	}
 	
 	public function parseData() {
-		$dataBase = explode(PHP_EOL, $this->fullData);
+		$fullDataArrayCommas = explode(PHP_EOL, $this->fullData);
+		$this->fullDataArray = array();
+		foreach ($fullDataArrayCommas as $record) {
+			$recordArray = explode(',', $record);
+			$myInstitution  = new Institution($recordArray);
+			$this->fullDataArray[] = $myInstitution;
+		}
 	}
 	
-	public function __construct () {
+	public function __construct() {
 		$this->getData();
 		$this->parseData();
+		$this->displayData();
 		
 	}
 	
+	public function displayData() {
+		$myHTML = <<<'EOT'
+	<html>
+		<head>
+			<title>Tarot Card Learner</title>
+			<script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
+			<script src='./learnTarot.js'></script>
+			<link rel="stylesheet" type="text/css" href="learnTarot.css">
+		</head>
+		<body>
+EOT;
+
+		foreach ($this->fullDataArray as $institution) {
+			$myHTML .= "<p>".$institution->name." ".$institution->control." ".$institution->stateCode." ".$institution->combinePolicySource."</p>";
+		}
+		
+		$myHTML = "</body></html>";
+		
+		echo $myHTML;
+	}
 
 }
 
+$myPage = new Tracker();
 
 ?>
