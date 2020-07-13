c08d45be658d09057a7e1d5778f88a53fbf957c1
arichshea <65359853+arichshea@users.noreply.github.com>
2020-07-12 21:43:56 -0400

    Added database and defined classes

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+
+class Institution {
+	
+	public $name;
+	public $stateCode;
+	public $policyType;
+	public $sourceLink;
+	public $locationCoordinates;
+	
+	public function __construct( $databaseArray ) {
+		$this->name = $databaseArray[0];
+		$this->stateCode = $databaseArray[1];
+		$this->policyType = $databaseArray[2];
+		$this->sourceLink = $databaseArray[3];
+		$this->getCoordinates();
+	}
+	
+	public function getCoordinates() {
+		
+	}
+	
+}
+
+
+
+class Tracker {
+	
+	
+	public function getData() {
+		$myfile = fopen("data-w8lLG.csv", "r") or die("Unable to open file!");
+		$myData = fread($myfile,filesize("data-w8lLG.csv"));
+		fclose($myfile);
+	}
+
+}
+
+
+?>
