8db596117cf4497e9ccf614c2c27d22e25dcea22
arichshea <65359853+arichshea@users.noreply.github.com>
2020-07-16 13:30:40 -0400

    Separated out classes

---
+++ statemap.php
@@ -0,0 +1,26 @@
+<?php
+
+class coronaStateMap {
+	
+	public $stateArray;
+	public $collegeArray;
+	public $stateCollegeArray;
+	public $coronaColor;
+	
+	public function __construct() {
+		
+	}
+	
+	public function getMap() {
+		
+	}
+	
+	public function addData( $data ) {
+		
+	}
+	
+}
+
+
+
+?>
